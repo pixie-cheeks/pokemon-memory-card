21d30ab1bc7f22bf938c283a0d1e0352de1d9cf9
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-10-06 21:54:17 +0530

    Implement card shuffle and score incrementing
    For shuffling cards, copied an algorithm from stackOverflow.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Game from './Game/Game.jsx';
 
-function Scoreboard() {
+function Scoreboard({ score }) {
   return (
     <div className="scoreboard">
       <div className="scoreboard__score">
         <div className="scoreboard__name">Score:</div>
-        <div className="scoreboard__count">0</div>
+        <div className="scoreboard__count">{score}</div>
       </div>
       <div className="scoreboard__score">
         <div className="scoreboard__name">High Score:</div>
@@ -17,13 +17,14 @@ function Scoreboard() {
 }
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
   return (
     <>
       <header className="header">
         <h1 className="page__title">Pokémon: Memory Card</h1>
-        <Scoreboard />
+        <Scoreboard score={currentScore} />
       </header>
-      <Game />
+      <Game {...{ setCurrentScore, currentScore }} />
     </>
   );
 }
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,19 +1,31 @@
 import { useState } from 'react';
 import pokemons from './game-data.json';
 
-function GameCard({ sprite, name }) {
+function GameCard({ sprite, name, onClick }) {
   return (
-    <div className="game__card card">
+    <button type="button" className="game__card card" onClick={onClick}>
       <div className="card__img-container">
         <img src={sprite} alt={name} className="img card__img" />
       </div>
       <div className="card__name">{name}</div>
-    </div>
+    </button>
   );
 }
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function shuffleArray(array) {
+  const arrayCopy = [...array];
+  for (let index = arrayCopy.length - 1; index >= 0; index--) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [arrayCopy[index], arrayCopy[randomIndex]] = [
+      arrayCopy[randomIndex],
+      arrayCopy[index],
+    ];
+  }
+  return arrayCopy;
 }
 
 function getRandomPokemon(pokemonList, maxPokemon = 15) {
@@ -34,17 +46,34 @@ const initialPokemons = pokemons.map((pokemon) => ({
   seen: false,
 }));
 
-function Game() {
-  const randomPokemon = getRandomPokemon(initialPokemons);
-  // const [seenPokemon, setSeenPokemon] = useState(randomPokemon);
+function Game({ setCurrentScore, currentScore }) {
+  const [pokemonCardList, setPokemonCardList] = useState(
+    getRandomPokemon(initialPokemons),
+  );
+  const [clickedPokemon, setClickedPokemon] = useState([]);
 
-  const pokemonCards = randomPokemon.map(({ sprite, name, id }) => (
-    <GameCard {...{ sprite, name }} key={id} />
+  const onClick = (clickedId) => {
+    if (clickedPokemon.includes(clickedId)) {
+      // Add lose condition here.
+      return;
+    }
+    setClickedPokemon([...clickedPokemon, clickedId]);
+    setCurrentScore((score) => score + 1);
+    if (clickedPokemon.length === pokemonCardList.length) {
+      // Add win condition here.
+      return;
+    }
+
+    setPokemonCardList(shuffleArray(pokemonCardList));
+  };
+
+  const pokemonCards = pokemonCardList.map(({ sprite, name, id }) => (
+    <GameCard {...{ sprite, name }} onClick={() => onClick(id)} key={id} />
   ));
 
   return (
     <div className="game">
-      <h2 className="game__card-count">0/15</h2>
+      <h2 className="game__card-count">{currentScore}/15</h2>
       <div className="game__cards-container">{pokemonCards}</div>
     </div>
   );
